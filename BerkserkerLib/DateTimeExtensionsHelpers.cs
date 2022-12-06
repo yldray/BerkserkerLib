432d1a54d673ceb599897da6a44150b8bf1df034
Yildiray <tasbasyildiray@outlook.com>
2022-12-06 12:24:01 +0300

    Add project files.

---
+++ BerkserkerLib/DateTimeExtensionsHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerkserkerLib
+{
+    public static class DateTimeExtensionsHelpers
+    {
+        public static DateTime FirstDayOfMonth(this DateTime dt) =>
+            new DateTime(dt.Year, dt.Month, 1);
+
+        public static DateTime FirstDayOfNextMonth(this DateTime dt) =>
+            dt.FirstDayOfMonth().AddMonths(1);
+        public static DateTime FirstDayOfWeek(this DateTime dt)
+        {
+            var culture = System.Threading.Thread.CurrentThread.CurrentCulture;
+            var diff = dt.DayOfWeek - culture.DateTimeFormat.FirstDayOfWeek;
+
+            if (diff < 0)
+            {
+                diff += 7;
+            }
+
+            return dt.AddDays(-diff).Date;
+        }
+
+        public static DateTime LastDayOfMonth(this DateTime dt) =>
+            dt.FirstDayOfMonth().AddMonths(1).AddDays(-1);
+
+        public static DateTime LastDayOfWeek(this DateTime dt) =>
+            dt.FirstDayOfWeek().AddDays(6);
+    }
+}
---
+++ BerkserkerTestUI/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BerkserkerLib;
+
+Console.WriteLine("Hello, World!");
+
+DateTime firstdayOfThisWeek = DateTime.Now.FirstDayOfWeek();
+Console.WriteLine(firstdayOfThisWeek.ToString());
+
+Console.ReadKey();
